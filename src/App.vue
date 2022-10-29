2a0b5c1e72a16829fa918bd8eb9996077b0b3fdf
buharisahith <buharisahith@gmail.com>
2022-10-29 18:22:20 +0530

    worked on header component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <HeaderVueComponent />
+    
+  </template>
+  
+  <script>
+  import HeaderVueComponent from "./components/Header.vue";
+  export default {
+    name: "App",
+    components: {
+      HeaderVueComponent,
+    },
+  };
+  </script>
+  
+  <style scoped></style>
